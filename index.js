eadbfd26207714cedd408684bc0b8041c7ed4716
saagor16 <saagor.16@gmail.com>
2024-04-30 02:50:57 +0600

    thired

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors =require('cors');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 
 app.use(cors());
